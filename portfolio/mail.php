9395dba92bf49129ef0422cdd7a3c052c2a8a60c
verstka80 <verstka80@gmail.com>
2016-07-19 10:50:55 +0300

    19-07 (10.50)

--- portfolio/mail.php
+++ portfolio/mail.php
@@ -1,11 +1,12 @@
 <?php
 
-$frm_name  = "Youname";
+$frm_name  = "Youname"
 $recepient = "youmail@mail.ru";
 $sitename  = "Портфолио фотографий";
 $subject   = "Новая заявка с сайта \"$sitename\"";
 
 $name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
-$message = "Телефон: $phone \nИмя: $name";
+$email = trim($_POST["email"]);
+$message = trim($_POST["message"]);
+$message = "E-mail: $email \nИмя: $name \nСообщение: $message";
 mail($recepient, $subject, $message, "From: $frm_name <$recepient>" . "\r\n" . "Reply-To: $recepient" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
