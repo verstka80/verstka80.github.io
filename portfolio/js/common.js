51e88edb274ff443417e15216de381e6846bdfbf
verstka80 <verstka80@gmail.com>
2016-07-19 10:56:11 +0300

    19-07 (11.00)

--- portfolio/js/common.js
+++ portfolio/js/common.js
@@ -1,106 +1,117 @@
 $(document).ready(function() {
 
-	$("body, .left_side").niceScroll({
-		horizrailenabled : false,
-		"verge" : "500"
-	});
+    //Подключаем niceScroll //
+    $("body, .left_side").niceScroll({
+        horizrailenabled: false,
+        "verge": "500"
+    });
 
-	$(".gallery").css("min-height", $(document).height()*1.1);
 
-	$(".btn_mnu").click(function() {
-		$(this).toggleClass("active");
-		$(".left_side").toggleClass("active");
+    // КНОПКА  //
+    $(".btn_mnu").click(function() {
+        $(this).toggleClass("active");
 
-	});
+        // чтобы left_side выезжал и заезжал при нажатиях на кнопку
+        $(".left_side").toggleClass("active");
+    });
 
-	$(".gallery img").lazyload({
-		effect : "fadeIn",
-		threshold : 1000
-	}).parent().hover(function() {
-		$(".gallery a").css("opacity", ".8");
-		$(this).css("opacity", "1");
-	}, function() {
-		$(".gallery a").css("opacity", "1");
-	});
 
-	var wall = new freewall(".gallery");
-	wall.reset({
-		selector: "a",
-		animate: true,
-		cellW: 150,
-		cellH: "auto",
-		gutterX : 5,
-		gutterY : 5,
-		onResize: function() {
-			wall.fitWidth();
-		}
-	});
+    // http://www.appelsiini.net/projects/lazyload     lazyload  в "" пишем класс с картинкаи  //
 
-	var images = wall.container.find("a");
-	images.find("img").load(function() {
-		wall.fitWidth();
-	});
+    $(".gallery img").lazyload({
+        effect: "fadeIn",
+        threshold: 1000
+    }).parent().hover(function() {
+        $(".gallery a").css("opacity", ".7");
+        $(this).css("opacity", "1");
+    }, function() {
+        // яркость по умолчанию
+        $(".gallery a").css("opacity", "1");
+    });
 
-	$(".filter_label").click(function() {
-		$(".filter_label").removeClass("active");
-		var filter = $(this).addClass("active").data("filter");
-		wall.filter(filter);
-		setTimeout(function() {
-			$(window).resize();
-			wall.fitWidth();
-		}, 400);
-	});
 
-	$(".gallery a").magnificPopup({
-		type : 'image',
-		gallery : {
-			enabled : true
-		},
-		removalDelay: 400,
-		mainClass: 'mfp-fade'
-	}).click(function() {
-		$("button.mfp-arrow").delay(1000).fadeIn();
-	});
+    // Плитки FREEWALL  //
+    var wall = new freewall(".gallery");
+    wall.reset({
+        selector: "a",
+        animate: true,
+        cellW: 150,
+        cellH: "auto",
+        gutterX: 5,
+        gutterY: 5,
+        onResize: function() {
+            wall.fitWidth();
+        }
+    });
+
+    var images = wall.container.find("a");
+    images.find("img").load(function() {
+        wall.fitWidth();
+    });
+    // Плитки FREEWALL - фильтрация  //
+    $(".filter_label").click(function() {
+        $(".filter_label").removeClass("active");
+        var filter = $(this).addClass("active").data("filter");
+        wall.filter(filter);
+        setTimeout(function() {
+            $(window).resize();
+            wall.fitWidth();
+        }, 400);
+    });
 
 
 
-	//Цели для Яндекс.Метрики и Google Analytics
-	$(".count_element").on("click", (function() {
-		ga("send", "event", "goal", "goal");
-		yaCounterXXXXXXXX.reachGoal("goal");
-		return true;
-	}));
-
-	//SVG Fallback
-	if(!Modernizr.svg) {
-		$("img[src*='svg']").attr("src", function() {
-			return $(this).attr("src").replace(".svg", ".png");
-		});
-	};
-
-	//Аякс отправка форм
-	//Документация: http://api.jquery.com/jquery.ajax/
-	$("#callback").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				
-			}, 1000);
-		});
-		return false;
-	});
+// magnificPopup  //
+    $(".gallery a").magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        },
+        removalDelay: 300,
+        mainClass: 'mfp-fade'
+    });
 
 
-	
+
+    //Цели для Яндекс.Метрики и Google Analytics
+    $(".count_element").on("click", (function() {
+        ga("send", "event", "goal", "goal");
+        yaCounterXXXXXXXX.reachGoal("goal");
+        return true;
+    }));
+
+    //SVG Fallback
+    if (!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function() {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    };
+
+    //Аякс отправка форм
+    //Документация: http://api.jquery.com/jquery.ajax/
+    $("#form").submit(function() {
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function() {
+            alert("Спасибо за заявку!");
+            setTimeout(function() {
+
+            }, 1000);
+        });
+        return false;
+    });
+
+
+
 });
+
 
 $(window).load(function() {
 
-	$(".loader_inner").fadeOut();
-	$(".loader").delay(400).fadeOut("slow");
+    //PRELOADER
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
 
 });
